e9241fcdaa5f0e00705d291778b225e913ccd51f
Jamie Pate <jpate@moscore.com>
2017-12-14 23:01:38 -0800

    Hack to double the size of the layers on a retina screen.
    see https://github.com/los-cocos/cocos/issues/303
    and
    https://bitbucket.org/pyglet/pyglet/issues/45/retina-display-scaling-on-os-x

--- game_scene.py
+++ game_scene.py
@@ -2,6 +2,12 @@
 Scene where all the acutal gameplay happens
 """
 from cocos.scene import Scene
+from cocos.director import director
+is_cocoa = True
+try:
+    from pyglet.window.cocoa import CocoaWindow
+except ImportError as ex:
+    is_cocoa = False
 from game_layer import GameLayer
 from background_layer import BackgroundLayer
 
@@ -14,4 +20,15 @@ class GameScene(Scene):
     """
     def __init__(self, *args):
         args = list(args) + [BackgroundLayer(), GameLayer()]
+        # hack for retina displays because pyglet fails
+        # see https://github.com/los-cocos/cocos/issues/303
+        # and https://bitbucket.org/pyglet/pyglet/issues/45/retina-display-scaling-on-os-x
+
+        if is_cocoa:
+            # todo: actually detect retina displays and set scale accordingly
+            for layer in args:
+                layer.anchor_x = 0
+                layer.anchor_y = 0
+                layer.scale = 2
+
         super().__init__(*args)
