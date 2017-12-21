ad6f8e3367d6fd5e906c32d94d23623c2c1c0a47
Jamie Pate <jpate@moscore.com>
2017-12-20 21:47:57 -0800

    Flesh out the grassy field
    Place grass closer together, with grass filling in the side and near
    margins.
    Add grass on the same y plane to batches to improve opengl performance.

--- game_layer.py
+++ game_layer.py
@@ -8,6 +8,7 @@ from actor import Actor
 from player import Player
 from item_config import item_types
 import config
+from random import randint
 
 
 class GameLayer(Layer):
@@ -27,15 +28,24 @@ class GameLayer(Layer):
             Actor(item_types["player3"], (250, 600)),
             Actor(item_types["player4"], (900, 600)),
         ]
+
         width, height = config.screen_size
-        grass_distance = 60
+        grass_distance = 50
+        x_margin, y_margin, width, height = (width // 2, grass_distance, width, 960)
         self.grass = [
-            Actor(item_types["grass" + str((x + y) % 3 + 1)], (x, y))
-            for x in range(0, width, grass_distance)
-            for y in range(0, height, grass_distance)
+            [
+                Actor(item_types["grass" + str(randint(1, 3))], (x, y))
+                for x in range(-x_margin, width + x_margin, grass_distance)
+            ]
+            for y in range(-y_margin, height, grass_distance)
         ]
-        for grass in self.grass:
-            self.add(grass, -grass.coord.y)
+        from cocos.batch import BatchNode
+        for grass_batch in self.grass:
+            batch = BatchNode()
+            for grass in grass_batch:
+                grass.alpha = 0.8
+                batch.add(grass, -grass.coord.y)
+            self.add(batch, -grass_batch[0].coord.y)
 
         for player in self.players:
             self.add(player, -player.coord.y)
@@ -48,8 +58,8 @@ class GameLayer(Layer):
         last_player_id = 0
 
         for controller in self.controllers:
-                controller.id = last_player_id
-                last_player_id += 1
+            controller.id = last_player_id
+            last_player_id += 1
 
         self.robots = [
             Actor(item_types["robot1"], (200, 300)),
