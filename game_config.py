854f9d51cd75ab9093b34da795b7fe347e89a06a
Jamie Pate <jpate@moscore.com>
2018-01-07 16:52:01 -0800

    Add gameconfig for the different players and slingshots

--- game_config.py
+++ game_config.py
@@ -1,22 +1,30 @@
 item_types = {
-    "player1": {"type": "player", "texture": "boy.png", "size": (78, 78)},
-    "player2": {"type": "player", "texture": "danny.png", "size": (78, 78)},
-    "player3": {"type": "player", "texture": "sally.png", "size": (78, 78)},
-    "player4": {"type": "player", "texture": "tanker.png", "size": (78, 78)},
+    "player1": {"type": "player", "texture": "boy.png", "size": (78, 78), "name": "Boy", "equip": "slingshot1"},
+    "player2": {"type": "player", "texture": "danny.png", "size": (78, 78), "name": "Danny", "equip": "slingshot2"},
+    "player3": {"type": "player", "texture": "sally.png", "size": (78, 78), "name": "Sally", "equip": "slingshot3"},
+    "player4": {"type": "player", "texture": "tanker.png", "size": (78, 78), "name": "Tanker", "equip": "slingshot4"},
+    "player4": {"type": "player", "texture": "smele.png", "size": (78, 78), "name": "Smiley", "equip": "slingshot5"},
+    "player4": {"type": "player", "texture": "melody.png", "size": (78, 78), "name": "Melody", "equip": "slingshot6"},
+
+    "slingshot1": {"type": "items", "texture": "slingshot_orange.png", "size": (30, 30), "name": "Slingshot"},
+    "slingshot2": {"type": "items", "texture": "slingshot_black.png", "size": (30, 30), "name": "Slingshot"},
+    "slingshot3": {"type": "items", "texture": "slingshot_red.png", "size": (30, 30), "name": "Slingshot"},
+    "slingshot4": {"type": "items", "texture": "slingshot_blue.png", "size": (30, 30), "name": "Slingshot"},
+    "slingshot5": {"type": "items", "texture": "slingshot_green.png", "size": (30, 30), "name": "Slingshot"},
+    "slingshot6": {"type": "items", "texture": "slingshot_brown.png", "size": (30, 30), "name": "Slingshot"},
+
+    "robot1": {"type": "robot", "texture": "metal_computer.png", "size": (50, 50), "name": "Metal Computer"},
+    "robot2": {"type": "robot", "texture": "squaredah.png", "size": (128, 128), "name": "Squaredah"},
+    "robot3": {"type": "robot", "texture": "squaredoo.png", "size": (140, 140), "name": "Squaredoo"},
 
 
-    "robot1": {"type": "robot", "texture": "metal_computer.png", "size": (50, 50)},
-    "robot2": {"type": "robot", "texture": "squaredah.png", "size": (128, 128)},
-    "robot3": {"type": "robot", "texture": "squaredoo.png", "size": (140, 140)},
+    "grass1": {"type": "environment", "texture": "grass1.png", "size": (150, 52), "name": "Grass"},
+    "grass2": {"type": "environment", "texture": "grass2.png", "size": (150, 60), "name": "Grass"},
+    "grass3": {"type": "environment", "texture": "grass3.png", "size": (150, 68), "name": "Grass"},
 
+    "castle1": {"type": "environment", "texture": "castle1.png", "size": (1024, 512), "name": "Castle"},
 
-    "grass1": {"type": "environment", "texture": "grass1.png", "size": (150, 52)},
-    "grass2": {"type": "environment", "texture": "grass2.png", "size": (150, 60)},
-    "grass3": {"type": "environment", "texture": "grass3.png", "size": (150, 68)},
-
-    "castle1": {"type": "environment", "texture": "castle1.png", "size": (1024, 512)},
-
-    "rock": {"type": "items", "texture": "fruitbowl.png", "size": (40, 40)},
+    "rock": {"type": "items", "texture": "fruitbowl.png", "size": (40, 40), "name": "Fruitbowl"},
 }
 
 game_board = {
